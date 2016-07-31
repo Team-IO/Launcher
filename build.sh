8e8d91891e63a8722ecf6218f70af3b8ae10e0ab
Oliver Kahrmann <oliver.kahrmann@gmail.com>
2016-07-31 22:38:25 +0200

    Team-IO branding

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 ./gradlew clean build
+
+mkdir -p ./build/libs/
+cp launcher-fancy/build/libs/*-all.jar ./build/libs/
+cp launcher-builder/build/libs/*-all.jar ./build/libs/
+cp launcher-bootstrap/build/libs/*-all.jar ./build/libs/
+cp launcher/build/libs/*-all.jar ./build/libs/
+cp creator-tools/build/libs/*-all.jar ./build/libs/
+
 read -p "Press any key to continue..."
